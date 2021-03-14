c68fbc252dff3ff48e9c4065b98aade15d0d1712
Zain Qureshi <zain@Zains-MacBook-Pro.local>
2021-03-14 19:34:33 +0500

    custom pipes with args

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,11 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
+  transform(value: any, targetUnits: string): any {
+    console.log(targetUnits);
     if (!value) {
       return '';
     }
-    return value * 1.60934;
-  }
 
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        throw new Error('Target Unit not suppoterd');
+    }
+  }
 }
